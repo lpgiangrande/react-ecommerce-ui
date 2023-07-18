1de48722a7240d7bc72776885266165fd8122163
Laura G <62339671+lpgiangrande@users.noreply.github.com>
2023-07-18 23:15:44 +0200

    Add to cart component - first shot

--- src/components/QuantityCounterWithAddToCartButton.jsx
+++ src/components/QuantityCounterWithAddToCartButton.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function QuantityCounter() {
+
+  const [ quantity, setQuantity ] = useState(0)
+
+  const decrementQuantity = () => {
+    if (quantity > 0) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  const incrementQuantity = () => {
+    setQuantity(quantity + 1);
+  };
+
   return (
-    <div>
-      <span>Counter</span><span>button</span>
+    <div className="row mt-4">
+    <div className="col">
+      <div className="d-inline-block me-5">
+        <button 
+          className='me-3'
+          onClick={decrementQuantity}>-</button> 
+          <span> {quantity}  </span>
+        <button 
+          className='ms-3'
+          onClick={incrementQuantity}>+</button>
+      </div>
+      <div className="d-inline-block">
+        <button onClick={incrementQuantity}>Add to cart</button>
+      </div>
     </div>
-  )
+  </div>
+  );
 }
