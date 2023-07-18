8d9ae44ce07a6be4281ede35fe1b6695b9740cb8
Laura G <62339671+lpgiangrande@users.noreply.github.com>
2023-07-18 23:27:48 +0200

    add to cart update

--- src/components/QuantityCounterWithAddToCartButton.jsx
+++ src/components/QuantityCounterWithAddToCartButton.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react';
 
 export default function QuantityCounter() {
-
-  const [ quantity, setQuantity ] = useState(0)
+  const [quantity, setQuantity] = useState(0);
+  const [addedToCart, setAddedToCart] = useState(false);
+  const [cartQuantity, setCartQuantity] = useState(0);
 
   const decrementQuantity = () => {
     if (quantity > 0) {
@@ -15,22 +15,28 @@ export default function QuantityCounter() {
     setQuantity(quantity + 1);
   };
 
+  const handleAddToCart = () => {
+    if (quantity > 0) {
+      setCartQuantity(quantity);
+      setAddedToCart(true);
+    }
+  };
+
   return (
     <div className="row mt-4">
-    <div className="col">
-      <div className="d-inline-block me-5">
-        <button 
-          className='me-3'
-          onClick={decrementQuantity}>-</button> 
-          <span> {quantity}  </span>
-        <button 
-          className='ms-3'
-          onClick={incrementQuantity}>+</button>
+      <div className="col">
+        <div className="d-inline-block me-5">
+          <button className="me-3" onClick={decrementQuantity}>-</button> 
+          <span>{quantity}</span>
+          <button className="ms-3" onClick={incrementQuantity}>+</button>
+        </div>
+        <div className="d-inline-block">
+          <button onClick={handleAddToCart} disabled={quantity === 0}>
+            Add to cart
+          </button>
+        </div>
       </div>
-      <div className="d-inline-block">
-        <button onClick={incrementQuantity}>Add to cart</button>
-      </div>
+      {addedToCart && <div>quantité ajoutée au panier : {cartQuantity}</div>}
     </div>
-  </div>
   );
 }
