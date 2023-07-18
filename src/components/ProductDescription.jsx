0f4e9d4a7c8dc4e9136b6e719ddb0bf695e68b63
Laura G <62339671+lpgiangrande@users.noreply.github.com>
2023-07-18 22:52:35 +0200

    ProductDescription component, UI

--- src/components/ProductDescription.jsx
+++ src/components/ProductDescription.jsx
@@ -3,17 +3,18 @@ import products from '../../products.json';
 import styles from "../assets/css/App.module.scss";
 
 export default function ProductDescription() {
-  const { productName, brand, description, price } = products.products[0];
+  const { productName, brand, description, currentPrice, discount, oldPrice } = products.products[0];
 
   return (
     <div>
       <h6 className={styles.brandName}>{brand}</h6>
       <h1 className={styles.productTitle}>{productName}</h1>
       <p className={styles.productDescription}>{description}</p>
-      <div>
-        <span className={styles.currentPrice}>$ {price}</span> - <span className={styles.brandName}> $discount tag</span>
+      <div className='pb-1 d-flex align-items-center'>
+        <span className={styles.currentPrice}>${currentPrice}</span>
+        <span className={styles.discountTag}>{discount} %</span>
       </div>
-      <span className={styles.oldPrice}>old price $</span>
+      <span className={styles.oldPrice}>$old price</span>
     </div>
   );
 }
