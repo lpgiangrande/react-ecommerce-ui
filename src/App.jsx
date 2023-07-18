b8c470a041caa0eacd3a8c5ac049dedf8eeca169
Laura G <62339671+lpgiangrande@users.noreply.github.com>
2023-07-18 14:55:49 +0200

    structure change

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import styles from "./assets/css/App.module.scss";
 import NavBar from './components/NavBar'
 import ProductSection from './components/ProductSection'
 import Carousel from './components/Carousel'
 import ProductDescription from './components/ProductDescription'
-import QuantityCounter from './components/QuantityCounter'
-import AddToCartButton from './components/AddToCartButton'
+import QuantityCounterWithAddToCartButton from './components/QuantityCounterWithAddToCartButton';
 import Footer from './components/Footer'
 
 function App() {
@@ -16,8 +13,7 @@ function App() {
         <ProductSection>
           <Carousel />
           <ProductDescription />
-          <QuantityCounter />
-          <AddToCartButton />
+          <QuantityCounterWithAddToCartButton />
         </ProductSection>
       <Footer />
     </div>
--- src/components/ProductSection.jsx
+++ src/components/ProductSection.jsx
@@ -3,20 +3,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from "../assets/css/App.module.scss";
 import Carousel from './Carousel';
 import ProductDescription from './ProductDescription';
-import QuantityCounter from './QuantityCounter';
-import AddToCartButton from './AddToCartButton';
+import QuantityCounterWithAddToCartButton from './QuantityCounterWithAddToCartButton';
 
 export default function ProductSection() {
   return (
-    <div className={`container product-section ${styles.content}`}>
+    <div className={`container d-flex justify-content-center align-items-center product-section ${styles.content}`}>
       <div className="row">
-        <div className="col-lg-6">
+      <div className="col-lg-6 ">
           <Carousel />
         </div>
-        <div className="col-lg-6">
+        <div className="col-lg-6 ">
           <ProductDescription />
-          <QuantityCounter />
-          <AddToCartButton />
+          <QuantityCounterWithAddToCartButton />
         </div>
       </div>
     </div>
